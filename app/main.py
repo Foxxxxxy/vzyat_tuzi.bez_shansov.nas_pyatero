36125cacd046ce86b637df16cf2e641ebaa2cc9d
BSaaber <maxim-movshin@yandex.ru>
2023-05-18 23:27:31 +0300

    init commit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from .schemas import *
+
 
 app = FastAPI()
 
@@ -11,3 +13,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+
+@app.get("/process/")
+async def say_hello(Data: MainInput):
+    return {"message": f"Hello world!"}
