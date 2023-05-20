81f9a2fa6be7247db1a211c272d652731a8814a6
BSaaber <maxim-movshin@yandex.ru>
2023-05-20 16:46:49 +0300

    set schemas for pdf process

---
+++ app/src/pieces/additional_service/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class AdditionalServiceCreationSchema(BaseModel):
+    name: str
+    average_price_dollar: float
+
+
+class AdditionalServiceSchema(AdditionalServiceCreationSchema):
+    id: int
--- app/src/pieces/calculation/schemas.py
+++ app/src/pieces/calculation/schemas.py
@@ -1,7 +1,29 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from app.src.pieces.additional_service.schemas import AdditionalServiceSchema
 from app.src.pieces.calculation.models import LegalEntityType
+from app.src.pieces.equipment.schemas import EquipmentSchema
+
+
+class EquipmentCalculationRequestSchema(BaseModel):
+    equipment_type_id: int
+    amount: int
+
+
+class EquipmentCalculationResponseSchema(BaseModel):
+    equipment: EquipmentSchema
+    amount: int
+    total_expenses: float
+
+
+class AdditionalServiceCalculationRequestSchema(BaseModel):
+    additional_service_type_id: int
+
+
+class AdditionalServiceCalculationResponseSchema(BaseModel):
+    additional_service: AdditionalServiceSchema
+    total_expenses: float
 
 
 class CalculationCreateFormSchema(BaseModel):
@@ -16,7 +38,7 @@ class CalculationCreateFormSchema(BaseModel):
 
     legal_entity_type: LegalEntityType
 
-    predicted_income_per_year_rub: int  # предполагаемый доход в год - для подсчета стоимости патента
+    predicted_income_per_year_rub: float  # предполагаемый доход в год - для подсчета стоимости патента
 
 
     # todo
@@ -28,3 +50,34 @@ class CalculationCreateFormSchema(BaseModel):
 
     # todo
     # иные потребности List[Потребность]
+
+class CalculationPreparedDataSchema(BaseModel):
+    # business info
+    industry_name: str
+    subindustry_name: str
+    legal_entity_type: str
+    district: str
+
+    # general
+    total_expenses: float
+
+    # employee
+    employee_amount: int
+    total_employee_expenses: float
+
+    # rent
+    building_area_size: float
+    land_area_size: float
+    total_rent_expenses: float
+
+    # taxes
+    predicted_income_per_year_rub: float
+    total_taxes_expenses: float
+
+    # equipment
+    equipments: list[EquipmentCalculationResponseSchema]
+    total_equipments_expenses: float
+
+    # additional services
+    additional_services: list[AdditionalServiceCalculationResponseSchema]
+    total_additional_services_expenses: float
